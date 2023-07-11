b98da69d0c8dd749867ba235e61fb2da3c38dcf5
Armash11 <124906592+Armash11@users.noreply.github.com>
2023-07-11 14:36:10 +0200

    fix2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,32 +3,40 @@ import throttle from 'lodash.throttle';
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
-const message = document.querySelector('.feedback-form textarea');
-const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('textarea[name="message"]');
+const email = document.querySelector('input[name="email"]');
 
 form.addEventListener('submit', onSubmitForm);
 form.addEventListener('input', throttle(onTextareaInput, 500));
+
 populateTextarea();
 
-const formData = {};
+let formData = {};
 
 function onSubmitForm(evt) {
   evt.preventDefault();
 
   console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
-  evt.currentTarget.reset();
-  localStorage.removeItem(LOCAL_STORAGE_KEY);
+
+  if (email.value === '' || message.value === '') {
+    return alert('Заповніть всі поля!');
+  }
+
+  evt.currentTarget.reset(); // метод очистки форми(сбрасує всі поля в начальне значення)
+
+  localStorage.removeItem(LOCAL_STORAGE_KEY); // очистка локалсториджа
 }
 
 function onTextareaInput(evt) {
   formData[evt.target.name] = evt.target.value;
+
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
 }
 
 function populateTextarea() {
   const savedMessage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
   if (savedMessage) {
-    email.value = savedMessage.email;
-    message.value = savedMessage.textarea;
+    form.email.value = savedMessage.email || '';
+    form.message.value = savedMessage.message || '';
   }
 }
