23657889fbc7a48fd8d6b83110a5fe907991e7f2
Armash11 <124906592+Armash11@users.noreply.github.com>
2023-07-12 16:32:02 +0200

    fix3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,34 +9,37 @@ const email = document.querySelector('input[name="email"]');
 form.addEventListener('submit', onSubmitForm);
 form.addEventListener('input', throttle(onTextareaInput, 500));
 
+const formData = {
+  email: '',
+  message: '',
+};
 populateTextarea();
-
-let formData = {};
 
 function onSubmitForm(evt) {
   evt.preventDefault();
 
-  console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
-
   if (email.value === '' || message.value === '') {
     return alert('Заповніть всі поля!');
   }
-
-  evt.currentTarget.reset(); // метод очистки форми(сбрасує всі поля в начальне значення)
-
+  JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? {};
+  evt.currentTarget.reset(); // метод очистки форми(сбрасує всі поля в початкове значення)
   localStorage.removeItem(LOCAL_STORAGE_KEY); // очистка локалсториджа
+  console.log(formData);
 }
 
 function onTextareaInput(evt) {
   formData[evt.target.name] = evt.target.value;
-
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
 }
 
 function populateTextarea() {
   const savedMessage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-  if (savedMessage) {
-    form.email.value = savedMessage.email || '';
-    form.message.value = savedMessage.message || '';
+  console.log(savedMessage);
+  if (savedMessage === null) {
+    return;
   }
+  email.value = savedMessage.email || '';
+  message.value = savedMessage.message || '';
+  formData.email = savedMessage.email || '';
+  formData.message = savedMessage.email || '';
 }
